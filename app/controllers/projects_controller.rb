a90e51cadcbe9c3abee7b6f5b9934612eb7c9a38
Igor Serpak <igor.serpak@gmail.com>
2014-08-30 11:46:46 -0300

    Created project actions (no testing, no partials)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,22 +4,44 @@ def index
   @projects = Project.all
 end
 
-def show
-#  id = 	params[:id]
-#  @project = Project.find(id)	
+def new
+  @project = Project.new
 end
 
-def new
-#  @project = Project.new
+def edit
+  @project = Project.find(params[:id])
+end
+
+def update
+  @project = Project.find(params[:id])
+  if @project.update_attributes(project_params)
+  	flash[:success] = "Project updated"
+  	redirect_to root_path
+  else
+  	render 'edit'
+  end
 end
 
 def create
-#  @project = Project.create!(params[:project])
-#  flash[:notice] = "#{@project.title} was successfully created."
+  @project = Project.new(project_params)
+  if @project.save
+  	flash[:notice] = "#{@project.title} was successfully created."
+  	redirect_to root_path
+  else
+    render 'new'
+  end
 end
 
 def destroy
-
+	Project.find(params[:id]).destroy
+    flash[:success] = "Project deleted."
+    redirect_to root_path
 end
+
+private
+	
+	def project_params
+	  params.require(:project).permit(:title)
+	end
 
 end
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,7 +1,6 @@
 namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
-    Project.create!(title: "Example User")
     Project.create!(title: "Garage")
     Project.create!(title: "Work")
     Project.create!(title: "Fly To The Moon")
@@ -9,5 +8,14 @@ namespace :db do
     Project.create!(title: "Bake A Cake")
     Project.create!(title: "Trip To Barselona")
     Project.create!(title: "A Lonely Project")
+
+    projects = Project.all
+    projects.each do |project|
+      5.times do
+        name = Faker::Lorem.sentence(5)
+        project.tasks.create!(name: name, status: "New", priority: 1, deadline: 10.days.from_now)
+      end
+    end
+
   end
 end
