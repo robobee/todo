f03487cb27fd990bcf45adcc94eae140ad3c0268
Igor Serpak <igor.serpak@gmail.com>
2014-08-31 10:37:30 -0300

    Fix problems with tasks validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ActiveRecord::Base
   belongs_to :project
-  default_scope -> { order(priority: :asc) } #add priority ordering
+  default_scope -> { order(priority: :asc) } 
   before_save :on_save
+  before_validation :on_validation
   validates :project_id, presence: true
   validates :name, presence: true, length: { maximum: 200 }
   validates :status, inclusion: { in: %w(New Done), message: "%{value} is not a valid status" }
@@ -13,8 +14,11 @@ class Task < ActiveRecord::Base
 
   protected
 
+  	def on_validation
+  	  self.status ||= "New"
+  	end
+
 	def on_save
-	  self.status ||= "New"
 	  if self.priority == nil
 	  then
 	  	max_priority = Task.where("project_id = #{self.project_id}").maximum("priority")
