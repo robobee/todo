c7867d28a1c27630e7c77e86b3c4e409753a9b7b
Igor Serpak <igor.serpak@gmail.com>
2014-08-31 18:19:23 -0300

    compile assets

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module Todo
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
