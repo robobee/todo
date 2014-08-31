1dde6af37a5d2f2082632dd0fc00de388f6910e8
Igor Serpak <igor.serpak@gmail.com>
2014-08-31 10:16:53 -0300

    Almost finished tasks actions

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,17 +1,27 @@
 class Task < ActiveRecord::Base
   belongs_to :project
-  default_scope -> { order(updated_at: :desc, priority: :asc) } #add priority ordering
-  after_initialize :init
+  default_scope -> { order(priority: :asc) } #add priority ordering
+  before_save :on_save
   validates :project_id, presence: true
   validates :name, presence: true, length: { maximum: 200 }
   validates :status, inclusion: { in: %w(New Done), message: "%{value} is not a valid status" }
   validates_each :deadline do |record, attr, value|
     if value && value <= DateTime.now
-    then record.errors.add(attr, 'must be in future') 
+    then record.errors.add(attr, 'must be set in future') 
     end
   end
-  def init 
-  	self.status ||= "New"
-  	self.priority ||= 1
-  end 
+
+  protected
+
+	def on_save
+	  self.status ||= "New"
+	  if self.priority == nil
+	  then
+	  	max_priority = Task.where("project_id = #{self.project_id}").maximum("priority")
+	  	if max_priority == nil
+	  	then self.priority = 1
+	  	else self.priority = max_priority + 1
+		end
+	  end
+	end
 end
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,19 +1,15 @@
 namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
-    Project.create!(title: "Garage")
-    Project.create!(title: "Work")
     Project.create!(title: "Fly To The Moon")
-    Project.create!(title: "Become The President Of The Universe")
     Project.create!(title: "Bake A Cake")
-    Project.create!(title: "Trip To Barselona")
-    Project.create!(title: "A Lonely Project")
-
+    Project.create!(title: "Plan Trip To Barselona")
+    
     projects = Project.all
     projects.each do |project|
       5.times do
         name = Faker::Lorem.sentence(5)
-        project.tasks.create!(name: name, status: "New", priority: 1, deadline: 10.days.from_now)
+        project.tasks.create!(name: name, status: "New")
       end
     end
 
